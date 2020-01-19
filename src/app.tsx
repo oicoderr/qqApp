852d2a1785c5d606110ec4553a0556bdd8edaf48
py.c <849656290@qq.com>
2020-01-19 11:22:24 +0800

    ～

--- src/app.tsx
+++ src/app.tsx
@@ -87,11 +87,11 @@ class _App extends Component {
 	componentDidMount() {
 		let _this = this;
 		// console.error = () => { };
-		// console.table = () => {};
-		// console.log = () => {};
-		// console.info = () => {};
-		// console.warn = () => {};
-		// console.dir = () => {};
+		console.table = () => {};
+		console.log = () => {};
+		console.info = () => {};
+		console.warn = () => {};
+		console.dir = () => {};
 
 		// 获取当前版本
 		configObj.getVersion();
